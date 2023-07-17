0098335de00a437e3912f0c9329dbf8927a437d4
sseon <rlatjsgur000@naver.com>
2023-07-17 22:28:04 +0900

    [add] layout & hidden component

--- codecamp-frontend-sseon/class/pages/_app.tsx
+++ codecamp-frontend-sseon/class/pages/_app.tsx
@@ -1,19 +1,21 @@
 import { AppProps } from 'next/app'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+// import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import 'antd/dist/antd.css';
 import Layout from '../src/components/commons/layout';
+import ApolloSetting from '../src/components/commons/apollo';
+import { Global } from '@emotion/react';
+import { globalStyles } from '../src/commons/styles/globalStyles';
 
 export default function App({ Component }: AppProps): JSX.Element {
-  const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache()
-  })
-
   return (
-    <ApolloProvider client={client}>
-      <Layout>
-        <Component />
-      </Layout>
-    </ApolloProvider>
-  )
+    <ApolloSetting>
+      <>
+        <Global styles={globalStyles} />
+        <div>qqqqqqqqq</div>
+        <Layout>
+            <Component />
+        </Layout>
+      </>
+    </ApolloSetting>
+  );
 }
--- codecamp-frontend-sseon/class/src/components/commons/layout/index.tsx
+++ codecamp-frontend-sseon/class/src/components/commons/layout/index.tsx
@@ -1,14 +1,39 @@
+import { useRouter } from "next/router"
+import LayoutBanner from "./banner"
+import LayoutFooter from "./footer"
+import LayoutHeader from "./header"
+import LayoutNavigation from "./navigation"
+
+/** router를 이용해 조건부 렌더링!!
+ * 헤더를 숨기고 싶은 페이지를 배열에 담고
+ * router path 값이 배열에 있는지 boolean 변수로 조건부 렌더링
+ */
+const HIDDEN_HEADERS = [
+    "/12-02-library-star",
+    // ...
+    // ...
+    // ...
+]
+
 interface IProps {
     children: JSX.Element
 }
 export default function Layout(props: IProps) {
+    const router = useRouter();
+    console.log(router.asPath);
+
+    const isHiddenHeader = HIDDEN_HEADERS.includes(router.asPath);
+
     return (
         <>
-            <div>여기는 헤더입니다</div>
-            <div>여기는 배너입니다</div>
-            <div>여기는 메뉴입니다</div>
-            <div>{props.children}</div>
-            <div>여기는 푸터입니다</div>
+            {!isHiddenHeader && <LayoutHeader />}
+            <LayoutBanner />
+            <LayoutNavigation />
+            <div style={{height: "500px", display: "flex"}}>
+                <div style={{width: "30%", backgroundColor: "orange"}}>사이드바</div>
+                <div style={{width: "70%"}}>{props.children}</div>
+            </div>
+            <LayoutFooter />
         </>
     )
 }
