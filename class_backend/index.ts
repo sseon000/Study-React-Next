82be3660ba2d305dd95886a91091a2162f6e7beb
sseon <rlatjsgur000@naver.com>
2023-08-07 22:17:48 +0900

    11-backend-board-prod-Delete

--- class_backend/index.ts
+++ class_backend/index.ts
@@ -11,27 +11,87 @@ import { Board } from './Board.postgres';
 
 // DOCS
 const typeDefs = gql`
+  input CreateBoardInput {
+    writer: String,
+    title: String,
+    contents: String
+  }
+
+  type UpdateBoardInput {
+    writer: String,
+    title: String,
+    contents: String
+  }
+
+  type MyBoard {
+    number: Int
+    writer: String
+    title: String
+    contents: String
+  }
+
+
   type Query {
-    fetchBoards: String
+    fetchBoards: [MyBoard]
   }
 
   type Mutation {
-    CreateBoard: String
+    # 연습용(example 방식)
+    # createBoard(writer: String, title: String, contents: String): String
+
+    # 실무용(backend 방식)                         !가 있으면 필수
+    createBoard(createBoardInput: CreateBoardInput!): String
+
+    updateBoard(id: String, updateBoardInput: UpdateBoardInput): String
+
+    deleteBoard(id: String): String
   }
 `;
 
 // API
 const resolvers = {
   Query: {
-    fetchBoards: () => {
-        return "게시글 조회에 성공하였습니다.";
+    fetchBoards: async () => {
+      // 모두 꺼내기
+      const result = await Board.find();
+      // 한개만 골라서 꺼내기
+      // Board.findOne({ where: { number: 3 } })
+      return result; // [{ number: 1, writer: "철수", title: "안녕하세요", contents: "반갑습니다" }, {}, {}, ...]
     },
   },
 
   Mutation: {
-    CreateBoard: () => {
-        return "게시글 등록에 성공하였습니다.";
-    }
+    // parent에는 프론트(브라우저)에서 api요청할때 인자는 args에 정보가 들어감,  백에서 백으로 api요청할때 인자는 parent에 정보
+    // context에는 request, header 정보
+    // info에는 graphql api 기타 정보
+    createBoard: async (parent: any, args: any, context: any, info: any) => {
+      await Board.insert({
+        ...args.createBoardInput
+
+        // 하나 하나 모두 입력하는 방식
+        // writer: args.createBoardInput.writer,
+        // title: args.createBoardInput.title,
+        // contents: args.createBoardInput.contents,
+      })
+
+      return "게시글 등록에 성공하였습니다.";
+    },
+
+    // updateBoard: async (parent: any, args: any, context: any, info: any) => {
+    //   await Board.update({ number: 3 }, { writer: "영희" }); // => 3번 게시글의 작성자를 영희로 바꿔줘
+    //   await Board.update({ args.number: 3 }, { writer: args.updateBoardInput.writer }); // => 3번 게시글의 작성자를 영희로 바꿔줘
+
+    //   return "게시글 수정에 성공하였습니다.";
+    // },
+
+    // deleteBoard: async () => {
+    //   await Board.delete({ number: 3 }); // => 3번 게시글을 삭제해줘
+    //   await Board.update({ nubmer: 3 }, { isDeleted: true }); // => 실무에서는 실제로 삭제하지 않고, isDeleted라는 컬럼이 true이면 삭제되었다고 가정
+    //   await Board.update({ number: 3 }, { deletedAt: new Date() }); // => 언제 deletedAt이 null이면 삭제 안된 게시글, new Date() 시간이들어가 있으면? 그 시간에 삭제된 게시글
+    //
+    //   return "게시글 삭제에 성공하였습니다.";
+    // },
+
   }
 };
 
