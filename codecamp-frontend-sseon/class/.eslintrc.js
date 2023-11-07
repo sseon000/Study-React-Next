7d92405a91faffab2c378e4742b7a4a353720fa3
sseon <rlatjsgur000@naver.com>
2023-11-07 22:24:54 +0900

    [ UI프레임워크 ] - Section13
    [antd] - 바퀴를 재발명 하지마라 : 시간절약, 버그 최소화
     -> 공식문서를 통한 라이브러리 습득력을 키우는게 중요!!
    
    [13-01-library-icon]
    pages/section13/13-01-library-icon/index.tsx
    *. https://ant.design/components/icon
    1. yarn add @ant-design/icons
    
    *. css 적용하는법
    1. 이모션 스타일 사용하기! : import styled from '@emotion/styled'
    2. 이벤트 핸들러 함수 : 이벤트 버블링을 활용해서 다른 부모태그로 감싸고 부모태그에 함수바인딩
    
    [13-02-library-star]
    pages/section13/13-02-library-star/index.tsx
    *. 기능은 잘 나오는데, css는 적용이 안될경우 공식홈페이지에서 css를 따로 다운로드 받아야함!!

--- codecamp-frontend-sseon/class/.eslintrc.js
+++ codecamp-frontend-sseon/class/.eslintrc.js
@@ -1,36 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "standard-with-typescript",
-        "prettier"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "project": "**/tsconfig.json",
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off",
-        "@typescript-eslint/space-before-blocks": "off",
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  overrides: [],
+  parserOptions: {
+    project: "**/tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-misused-promises": "off", // HOF를 배우고 나서 wrapAsync 함수로 해결 가능 (그 전까지는 off 할 예정)
+    "@typescript-eslint/space-before-blocks": "off",
+  },
+};
