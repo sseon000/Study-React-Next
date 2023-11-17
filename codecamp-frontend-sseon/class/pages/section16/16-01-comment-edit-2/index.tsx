b9c2401b0ce6381114bf31f076e8ab2a2c4d3696
sseon <rlatjsgur000@naver.com>
2023-11-18 08:19:55 +0900

    [ 무한스크롤 ] - Section16
    [16-01-comment-edit-1]
    pages/section16/16-01-comment-edit-1/index.tsx
     - index state를 선언하고, 클릭 버튼에 id를 index줌 > 온클릭시 해당 id index를 state값으로 변경시키고 해당 index와 배열 index가 같은거만 input(수정창)으로 변경
     - 수정을 하나 밖에 못함 >> edit-2
    
    [16-01-comment-edit-2]
    pages/section16/16-01-comment-edit-2/index.tsx
     - state를 배열로 선언
     - 클릭 이벤트에서 배열로 선언한 배열 state를 복사하고 해당 id index를 true로 변경 > 배열의 값은 바뀌는데 리렌더링이 안일어남
       > 주소값 자체를 복사하는거라 원본 자체가 바뀜 > setState하기전에 원본이 바뀌었기 때문에 리렌더링이 안일어남
       > 얕은복사 해야함(리액트는 불변성을 지켜야함) : 불변성이란? 원본은 변경되지 않게 새로운 메모리 공간을 만들어 값만 복사함
       > 한 컴포넌트에서 배열로 state를 관리하면 유지보수성이 떨어짐 > edit-3
    
    [16-01-comment-edit-3]
    pages/section16/16-01-comment-edit-3/index.tsx
    src/components/units/16-comment-item/index.tsx
     - 배열의 item을 각 컴포넌트로 분리해서 item하나마나 각각 state가 만들어짐!

---
+++ codecamp-frontend-sseon/class/pages/section16/16-01-comment-edit-2/index.tsx
@@ -0,0 +1,65 @@
+import { useQuery, gql } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+import type { MouseEvent } from "react";
+import { useState } from "react";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingPaMovedge(): JSX.Element {
+  const [myIndex, setMyIndex] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const { data } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(
+    FETCH_BOARDS
+  );
+
+  const onClickEdit = (event: MouseEvent<HTMLButtonElement>): void => {
+    // 주소값 자체를 복사하는거라 원본 자체가 바뀜 > setState하기전에 원본이 바뀌었기 때문에 리렌더링이 안일어남
+    // const qqq = myIndex;
+    // 얕은복사 해야함(리액트는 불변성을 지켜야함) : 불변성이란? 원본은 변경되지 않게 새로운 메모리 공간을 만들어 값만 복사함
+    const qqq = [...myIndex];
+    qqq[Number(event.currentTarget.id)] = true;
+    setMyIndex(qqq);
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el, index) =>
+        !myIndex[index] ? (
+          <div key={el._id}>
+            <span style={{ margin: "10px" }}>{el.title}</span>
+            <span style={{ margin: "10px" }}>{el.writer}</span>
+            <button id={String(index)} onClick={onClickEdit}>
+              수정하기
+            </button>
+          </div>
+        ) : (
+          <input type="text" key={el._id} />
+        )
+      )}
+    </div>
+  );
+}
