e8e781ad54b223130111d2db2ec9752adbc174f0
sseon <rlatjsgur000@naver.com>
2023-11-27 21:52:02 +0900

    [ 백엔드의 이해 ] - Section18 1. 백엔드 API 나도 만들 수 있다고! 2. 서버! 제대로 이해했어! 3. 프론트엔드 개발자를 위한 백엔드 라이브러리가 있다고!?
    [ 18-게시판 API 만들기 실습 ]
    index.ts
    
    http://localhost:4000/grahql
    
    const server = new ApolloServer({
      typeDefs,
      resolvers,
    });
    
    // API 만들기
    const resolvers = {
      Query: {
        fetchBoard: async () => {
          // 1. 모두 꺼내기
          const result = await Board.find();
          console.log(result);
    
          // 2. 한개만 꺼내기
          // const result = await Board.findOne( { where: { number: 3 } });
          console.log(result);
    
          return result;
        },
      },
    
      Mutation: {
        createBoard: async (parent: any, args: any, context: any, info: any) => {
          // 연습용
          // await Board.insert({
          //   writer: args.writer,
          //   title: args.title,
          //   contents: args.contents,
          // });
    
          await Board.insert({
            ...args.createBoardInput,
    
            // 하나 하나 모두 입력하는 비효율적인 방식
            // writer: args.createBoardInput.writer,
            // title: args.createBoardInput.title,
            // contents: args.createBoardInput.contents,
          });
    
          return "게시글 등록에 성고했어요!!";
        },
    
        /*
        updateBoard: async () => {
          // 3번 게시글을 영희로 바꿔줘!
          await Board.update({ number: 3 }, { writer: "영희" });
        },
    
        deleteBoard: async () => {
          // 3번 게시글 삭제해줘!
          await Board.delete({ number: 3 });
          // 3번 게시글 삭제했다고 치자!! (소프트삭제) => fetchBoard시 isDeleted: false
          await Board.update({ number: 3 }, { isDeleted: true });
          // 3번 게시글 삭제했다고 치자!! (소프트삭제) => fetchBoard시 deletedAt: null
          await Board.update({ number: 3 }, { deletedAt: new Date() });
        },
        */
      },
    };
    
    1. Mutaion, Query 사용
    2. 함수의 파라미터로 parent, args, context, info 사용
    parent: 부모 타입 리졸버에서 반한된 결과를 가진 객체( from api to api )
    args: 요청 파라미터를 가진 객체
    context: GraphQL의 모든 리졸버가 공유하는 객체
    info: 명령 실행 상태 정보를 가진 객체
    
    // API-DOCS 만들기
    const typeDefs = `#graphql
      # args는 input!
      input CreateBoardInput {
        writer: String
        title: String
        contents: String
      }
    
      # return은 type!
      type MyBoard {
        number: Int
        writer: String
        title: String
        contents: String
      }
    
      type Query {
        fetchBoard: [MyBoard]
      }
    
      type Mutation {
        # 연습용
        # createBoard(writer: String, title: String, contents: String): String
    
        # 실무용
        createBoard(createBoardInput: CreateBoardInput): String
      }
    `;
    
    1. return type은 type 명령어 사용
    2. args type은 input 명령어 사용
    3. 기본 제공 type은 ID, Int, String, Boolean, array
    4. 객체는 새로운 타입을 만들어줘야함 ex) MyBoard, CreateBoardInput
    5. [return] - 배열 안에 1개 이상의 객체 ex) 게시판 전체 조회
    6. return - 객체 1개 ex) 특정 게시글 조회
    7. 묶음으로 한 번에 보내기 가능!

--- codecamp-frontend-sseon/class_backend/index.ts
+++ codecamp-frontend-sseon/class_backend/index.ts
@@ -14,15 +14,86 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 
 // API-DOCS 만들기
 const typeDefs = `#graphql
+  # args는 input!
+  input CreateBoardInput {
+    writer: String
+    title: String
+    contents: String
+  }
+
+  # return은 type!
+  type MyBoard {
+    number: Int
+    writer: String
+    title: String
+    contents: String
+  }
+
   type Query {
-    hello: String
+    fetchBoard: [MyBoard]
+  }
+
+  type Mutation {
+    # 연습용
+    # createBoard(writer: String, title: String, contents: String): String
+
+    # 실무용
+    createBoard(createBoardInput: CreateBoardInput): String
   }
 `;
 
 // API 만들기
 const resolvers = {
   Query: {
-    hello: () => "world",
+    fetchBoard: async () => {
+      // 1. 모두 꺼내기
+      const result = await Board.find();
+      console.log(result);
+
+      // 2. 한개만 꺼내기
+      // const result = await Board.findOne( { where: { number: 3 } });
+      console.log(result);
+
+      return result;
+    },
+  },
+
+  Mutation: {
+    createBoard: async (parent: any, args: any, context: any, info: any) => {
+      // 연습용
+      // await Board.insert({
+      //   writer: args.writer,
+      //   title: args.title,
+      //   contents: args.contents,
+      // });
+
+      await Board.insert({
+        ...args.createBoardInput,
+
+        // 하나 하나 모두 입력하는 비효율적인 방식
+        // writer: args.createBoardInput.writer,
+        // title: args.createBoardInput.title,
+        // contents: args.createBoardInput.contents,
+      });
+
+      return "게시글 등록에 성공했어요!!";
+    },
+
+    /*
+    updateBoard: async () => {
+      // 3번 게시글을 영희로 바꿔줘!
+      await Board.update({ number: 3 }, { writer: "영희" });
+    },
+
+    deleteBoard: async () => {
+      // 3번 게시글 삭제해줘!
+      await Board.delete({ number: 3 });
+      // 3번 게시글 삭제했다고 치자!! (소프트삭제) => fetchBoard시 isDeleted: false
+      await Board.update({ number: 3 }, { isDeleted: true });
+      // 3번 게시글 삭제했다고 치자!! (소프트삭제) => fetchBoard시 deletedAt: null
+      await Board.update({ number: 3 }, { deletedAt: new Date() });
+    },
+    */
   },
 };
 
@@ -31,13 +102,18 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+
+  // 선택한 사이트만 풀어주고 싶을때
+  // cors: {
+  //   origin: ["http//naver.com", "http://coupang.com"],
+  // }
 });
 
 // 정보는 비밀!
 const AppDataSource = new DataSource({
   type: "postgres",
   host: "",
-  port: 1521,
+  port: 1234,
   username: "",
   password: "",
   database: "",
