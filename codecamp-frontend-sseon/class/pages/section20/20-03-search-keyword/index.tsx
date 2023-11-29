7b12e0189db34e45259be75b6e4b357e5b13e670
sseon <rlatjsgur000@naver.com>
2023-11-29 20:19:44 +0900

    [ 검색 기능 구현 ] - Section20 1. 검색?! 알고리즘에서 배운 검색 알고리즘을 써야 하는 거야?? NO -> DB조회 검색 2. 검색 버튼이 없이 검색할 수 있다고!?
    [ 20-01-search ]
    pages/section20/20-01-search/index.tsx
    - db sql문 조건사용!!
    - 성능 향상을 위해 구글 검색엔진이 검색 키워드(token)들을 모아 역인덱스(Inverted-Index) 방식을 만들었다고함
    - 위 일을 해주는 엘라스틱서치가 생김
     > db, 엘라스틱서치  DISK I/O가 발생해서 성능이 느림
    - 성능이 향상된 Redis(key, value store) 생겨남 : 메모리 기반 데이터베이스, 캐싱기능
    ----------------- 서비스 규모가 큰 경우에 해당함, 규모가 작을 땐 오버엔지니어링임 ----------------------
    
    [ 20-02-search-debouncing ]
    pages/section20/20-02-search-debouncing/index.tsx
    package.json
    버튼없이 검색하기
    - 디바운싱 : 특정 시간 이내, 추가 입력 없을시, 마지막 1회만 실행
    - 쓰로틀링 : 특정 시간 이내, 추가 입력 있어도, 처음 1회만 실행
    lodash 라이브러리 설치 : yarn add lodash, yarn add --dev @types/lodash
    
    [ 20-03-search-keyword ]
    pages/section20/20-03-search-keyword/index.tsx
    package.json
    문장 : 철수가 점심을 먹었습니다.
    키워드 : 점심
    let tempArry = 문장.split(키워드);
    1. 키워드 중심으로 문장 자르기
     - 키워드에 시크릿코드를 더하고, 시크릿코드로 자르기
    2. 키워드 저장할 state 만들기
    3. 분리된 문장 map으로 그리고, el이 키워드랑 같으면 색 변경
     - map을 위한 uuid 라이브러리 설치 : yarn add uuid, yarn add --dev @types/uuid

---
+++ codecamp-frontend-sseon/class/pages/section20/20-03-search-keyword/index.tsx
@@ -0,0 +1,82 @@
+import { useQuery, gql } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+import { ChangeEvent, MouseEvent, useState } from "react";
+import _ from "lodash";
+import { v4 as uuidv4 } from "uuid";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int, $search: String) {
+    fetchBoards(page: $page, search: $search) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingPaMovedge(): JSX.Element {
+  // const [search, setSearch] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  // 검색에서 refetch할 때, search 검색어가 refetch에 이미 저장되어 있는 상태이므로 추가로 search 포함하지 않아도 됨
+  const onClickPage = (event: MouseEvent<HTMLSpanElement>): void => {
+    void refetch({ page: Number(event.currentTarget.id) });
+  };
+
+  const getDebounce = _.debounce((value) => {
+    void refetch({ search: value, page: 1 });
+    setKeyword(value);
+    console.log(value);
+  }, 500);
+
+  const onChangeSearch = (event: ChangeEvent<HTMLInputElement>): void => {
+    // setSearch(event.currentTarget.value);
+    getDebounce(event.currentTarget.value);
+  };
+
+  // const onClickSearch = (): void => {
+  //   void refetch({ search, page: 1 });
+  // };
+
+  return (
+    <div>
+      검색어 입력: <input type="text" onChange={onChangeSearch} />
+      {/* <button onClick={onClickSearch}>검색하기</button> */}
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span>
+            <input type="checkbox" />
+          </span>
+          <span style={{ margin: "10px" }}>
+            {el.title
+              .replaceAll(keyword, `@#$${keyword}@#$`)
+              .split("@#$")
+              .map((el) => (
+                <span
+                  key={uuidv4()}
+                  style={{ color: el === keyword ? "red" : "black" }}
+                >
+                  {el}
+                </span>
+              ))}
+          </span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+      {new Array(10).fill(1).map((_, index) => (
+        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
+          {index + 1}
+        </span>
+      ))}
+    </div>
+  );
+}
