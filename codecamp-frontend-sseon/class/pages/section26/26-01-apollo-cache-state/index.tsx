a6d6dfb2fc04d3a903e5efc5b3d3c8d97407fce9
sseon <rlatjsgur000@naver.com>
2023-12-26 21:06:32 +0900

    [ apollo-cache-state ] - Section26 1. 너 아직도 리페치 쓰니!?
    [ 26-01-apollo-cache-state ]
    pages/section26/26-01-apollo-cache-state/index.tsx
    * 캐시 직접 수정 이해
    - 리페치 vs 캐시수정
    - 리페치(네트워크 요청) : 아폴로캐시 + 리코일 + 브라우저 저장소
    - 캐시수정 : 아폴로캐시에 직접추가(글로벌스테이트가 변경되면 리렌더되므로 리페치 없어져서 네트워크 요청을 안해도됨-비용절감)

---
+++ codecamp-frontend-sseon/class/pages/section26/26-01-apollo-cache-state/index.tsx
@@ -0,0 +1,108 @@
+import { useQuery, gql, useMutation } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation ($boardId: ID!) {
+    deleteBoard(boardId: $boardId) {
+      message
+    }
+  }
+`;
+
+const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingPaMovedge(): JSX.Element {
+  const { data } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(
+    FETCH_BOARDS
+  );
+
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+  const [mutationFunc] = useMutation(CREATE_BOARD);
+
+  interface IPrev {
+    __ref: string;
+  }
+  const onClickDelete = (boardId: string) => (): void => {
+    void deleteBoard({
+      variables: { boardId },
+      // refetchQueries: [{ query: FETCH_BOARDS }],
+      update(cache, { data }) {
+        cache.modify({
+          fields: {
+            fetchBoards: (prev: IPrev[], { readField }) => {
+              const deletedId = data.deleteBoard; // 삭제 완료된 ID
+              const filteredPrev = prev.filter(
+                (el) => readField("el._id", el) !== deletedId
+              );
+              return [...filteredPrev]; // 삭제된 ID를 제외한 나머지 9개만 리턴
+            },
+          },
+        });
+      },
+    });
+  };
+
+  const onClickSubmit = (): void => {
+    void mutationFunc({
+      variables: {
+        createBoardInput: {
+          writer: "철수",
+          password: "1234",
+          title: "제목입니다~~",
+          contents: "내용입니다@@",
+        },
+      },
+      // refetchQueries: [{ query: FETCH_BOARDS }],
+      // update(cache, response) { response가 객체라 { data } 구조분해할당 형식으로 받을 수 있음
+      // 캐시를 직접수정하는 방식(글로벌 스테이트 캐시)
+      update(cache, { data }) {
+        cache.modify({
+          fields: {
+            fetchBoards: (prev) => {
+              return [data.createBoard, ...prev];
+            },
+          },
+        });
+      },
+    });
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span>
+            <input type="checkbox" />
+          </span>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+          <button onClick={onClickDelete(el._id)}>삭제하기</button>
+        </div>
+      ))}
+      <button onClick={onClickSubmit}>등록하기</button>
+    </div>
+  );
+}
